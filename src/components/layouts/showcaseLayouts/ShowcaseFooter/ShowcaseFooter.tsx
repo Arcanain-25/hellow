056afaa9c15db914efdfed6a95bf3c00e547be8a
Arcanain-25 <kovalciuks@gmail.com>
2025-09-11 23:52:32 +0300

    мизерные изменения

--- src/components/layouts/showcaseLayouts/ShowcaseFooter/ShowcaseFooter.tsx
+++ src/components/layouts/showcaseLayouts/ShowcaseFooter/ShowcaseFooter.tsx
@@ -4,4 +4,4 @@ const ShowcaseFooter: React.FC = () => {
   return <footer className={classes.footer}>© 2025-2026 React e-commerce (admin:Бог смерти)</footer>;
 };
 
-export default ShowcaseFooter;
+export default ShowcaseFooter;  
